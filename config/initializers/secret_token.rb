1076b31c0fc9969db8267e092a07bec8d5eef1b4
Markus Nordin <markus.nordin@gmail.com>
2011-09-24 15:30:00 +0200

    Added User model, empty controller and views with jQuery mobile framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpToSpeed::Application.config.secret_token = 'a1c11c8f46b8dfcc5d77bc83ac9fe9cfa8e7869e6f0ce4cb074f24cde088ee0d83cc7e4157fa70204f59b981b190e4b781412c68bb3ac61262c486ca751288ac'
