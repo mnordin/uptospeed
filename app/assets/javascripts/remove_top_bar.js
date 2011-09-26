916a6876f9cb10bb508de06e128f887acc7d52a7
Markus Nordin <markus.nordin@gmail.com>
2011-09-26 11:27:44 +0200

    Trying a new if statement to verify that user agent is a phone

--- app/assets/javascripts/remove_top_bar.js
+++ app/assets/javascripts/remove_top_bar.js
@@ -1,10 +1,13 @@
 // Taken from http://davidwalsh.name/hide-address-bar
 
-// When ready...
-window.addEventListener("load",function() {
-  // Set a timeout...
-  setTimeout(function(){
-    // Hide the address bar!
-    window.scrollTo(0, 1);
-  }, 0);
-});
+//For iPhone and Andriod To remove Address bar when viewing website on Safari Mobile
+if((Browser.Platform.ios) || (Browser.Platform.android) && (Browser.safari)) {
+    // When ready...
+    window.addEventListener("load",function() {
+        // Set a timeout...
+        setTimeout(function(){
+            // Hide the address bar!
+            window.scrollTo(0, 1);
+        }, 0);
+    });
+} 
