6419b5fead865648cdabc8a971340903ccb64bdc
Markus Nordin <markus.nordin@gmail.com>
2011-09-26 11:07:34 +0200

    Added rules for removing the top bar in iOS browser

---
+++ app/assets/javascripts/remove_top_bar.js
@@ -0,0 +1,10 @@
+// Taken from http://davidwalsh.name/hide-address-bar
+
+// When ready...
+window.addEventListener("load",function() {
+  // Set a timeout...
+  setTimeout(function(){
+    // Hide the address bar!
+    window.scrollTo(0, 1);
+  }, 0);
+});
