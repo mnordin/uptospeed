b163d4e0ca66b1eb2230d62c6370e3b269dd9056
Markus Nordin <markus.nordin@gmail.com>
2011-09-27 16:13:57 +0200

    Added functions to create and fetch a user from google auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  #protect_from_forgery
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      require_authed_user
+    end
+  end
+
+  private
+  def require_authed_user
+    redirect_to "/auth/google" if session[:user_id].nil?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
+  has_many :authentications, :dependent => :destroy
+
   validates :email, :presence => true, :uniqueness => true
+
+  def self.find_or_create_by_omniauth_hash(hash)
+    #omniauth_hash["user_info"]["email"]
+    return User.find_by_email(hash["user_info"]["email"]) || User.create_from_hash(hash)
+  end
+
+  private
+  def self.create_from_hash(hash)
+    User.create(
+      :first_name => hash["user_info"]["first_name"],
+      :last_name => hash["user_info"]["last_name"],
+      :email => hash["user_info"]["email"]
+    )
+  end
 
 end
