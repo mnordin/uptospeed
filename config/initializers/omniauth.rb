f8daa2962cfe056267e90e267bc838be65b9a583
Markus Nordin <markus.nordin@gmail.com>
2011-09-29 15:42:05 +0200

    Forgot to add identifier key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.configuration.middleware.use OmniAuth::Builder do
   provider :open_id, nil, {
         :name => "google",
         :callback_path => "/auth/google/callback",
+        :identifier => "https://www.google.com/accounts/o8/id",
         :domain => 'newsdesk.se',
         :consumer_key => "uptospeed.se",
         :consumer_secret => "g_ysByjed_wnqipVx2bk8Quf",
