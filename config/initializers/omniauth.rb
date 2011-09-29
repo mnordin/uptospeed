927b9677b87a63198ebecc7de0af663d86417866
Markus Nordin <markus.nordin@gmail.com>
2011-09-29 14:45:14 +0200

    Changed omniauth google provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.configuration.middleware.use OmniAuth::Builder do
-  provider :openid, nil, :name => 'google', :identifier =>'https://www.google.com/accounts/o8/id', :callback_path => "/auth/google/callback", :domain => 'newsdesk.se'
+  provider :openid, "uptospeed.se", "g_ysByjed_wnqipVx2bk8Quf", :name => 'google', :identifier =>'https://www.google.com/accounts/o8/id', :callback_path => "/auth/google/callback", :domain => 'newsdesk.se', :scope => "https://www.google.com/calendar/feeds/"
 end
