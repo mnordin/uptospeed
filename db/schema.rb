1a509fb1afc7151b495c10c10e1daf34dca2ad01
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 11:45:46 +0200

    Google apps don't store first and last name. Updated User model to just store name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110930093906) do
+ActiveRecord::Schema.define(:version => 20110930094342) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -29,14 +29,13 @@ ActiveRecord::Schema.define(:version => 20110930093906) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "email"
     t.text     "personal_goal"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
 end
