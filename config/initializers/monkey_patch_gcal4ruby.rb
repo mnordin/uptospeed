41bbd8fb219c23981bec5321df7e6d7ecd3d9fd6
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 16:27:28 +0200

    Forgot to require base and calendar

--- config/initializers/monkey_patch_gcal4ruby.rb
+++ config/initializers/monkey_patch_gcal4ruby.rb
@@ -1,4 +1,7 @@
 # Monkey patching GCal4Ruby authenticate method to take the auth token and email as params (instead of email and password and resolving the auth token)
+require 'gcal4ruby/base'
+require 'gcal4ruby/calendar'
+
 module GCal4Ruby
   class Service < Base
 
