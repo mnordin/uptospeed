f383061afef902b1d056e4a47735c70b7189a24e
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 14:05:21 +0200

    Added the failure route and debug controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,16 @@ class SessionsController < ApplicationController
     redirect_to root_url, :notice => "Signed in!"
   end
 
+  # Copy from create action
+  def failure
+    auth = request.env["omniauth.auth"]
+    Rails.logger.info("********#{auth}")
+    user = User.find_by_provider_and_uid("google", auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    store_google_credentials(auth)
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
   def destroy
     session[:user_id] = nil
     session[:access_token] = nil
