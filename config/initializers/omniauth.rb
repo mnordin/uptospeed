b810d91949945b1274d8fdbad5bef4aab2cfe889
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 10:57:39 +0200

    more keys in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
 Rails.configuration.middleware.use OmniAuth::Builder do
   provider :google, "newsdesk.se", "g_ysByjed_wnqipVx2bk8Quf", {
     :callback_path => "/auth/google/callback",
+    :identifier => "https://www.google.com/accounts/o8/id",
+    :client_id => "9715209208.apps.googleusercontent.com",
+    :client_secret => "iVYLH-oPgUPqI5JzLp6bcoKv",
     :scope => "https://www.google.com/calendar/feeds/"
   }
 end
