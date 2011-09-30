8e4431cce38356bfb14fa345bfd131adfd5e8aaf
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 11:03:07 +0200

    Rewrote omniauth initializer altogether

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,5 @@
 OmniAuth.config.full_host = "http://uptospeed.se"
 
-Rails.configuration.middleware.use OmniAuth::Builder do
-  provider :google, "newsdesk.se", "g_ysByjed_wnqipVx2bk8Quf", {
-    :callback_path => "/auth/google/callback",
-    :identifier => "https://www.google.com/accounts/o8/id",
-    :client_id => "9715209208.apps.googleusercontent.com",
-    :client_secret => "iVYLH-oPgUPqI5JzLp6bcoKv",
-    :scope => "https://www.google.com/calendar/feeds/"
-  }
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google, "newsdesk.se", "g_ysByjed_wnqipVx2bk8Quf", :scope => "https://www.google.com/calendar/feeds/" 
 end
