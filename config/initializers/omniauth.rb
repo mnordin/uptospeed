c3dd04604e761d8e736c5e4bd124b024cb9f1d52
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 14:18:36 +0200

    Removing httpS from all the identifiers and scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.full_host = "http://uptospeed.se"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google, "uptospeed.se", "g_ysByjed_wnqipVx2bk8Quf", :scope => "https://www.google.com/calendar/feeds/", :domain => "newsdesk.se", :identifier => "https://www.google.com/accounts/o8/id", :callback_path => "/auth/google/callback"
-  provider :google_apps, nil, :scope => "https://www.google.com/calendar/feeds/", :domain => "newsdesk.se", :identifier => "https://www.google.com/accounts/o8/id", :callback_path => "/auth/google_apps/callback"
+  provider :google, "uptospeed.se", "g_ysByjed_wnqipVx2bk8Quf", :scope => "http://www.google.com/calendar/feeds/", :domain => "newsdesk.se", :identifier => "http://www.google.com/accounts/o8/id", :callback_path => "/auth/google/callback"
+  provider :google_apps, nil, :scope => "http://www.google.com/calendar/feeds/", :domain => "newsdesk.se", :identifier => "http://www.google.com/accounts/o8/id", :callback_path => "/auth/google_apps/callback"
 end
