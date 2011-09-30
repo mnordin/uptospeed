d982081b549274839e6029e06e7fa5b0a830ae4a
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 10:51:44 +0200

    syntax error fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.configuration.middleware.use OmniAuth::Builder do
-  provider :google_apps, "newsdesk.se", "g_ysByjed_wnqipVx2bk8Quf" {
+  provider :google_apps, "newsdesk.se", "g_ysByjed_wnqipVx2bk8Quf", {
         :callback_path => "/auth/google_apps/callback",
         :scope => "https://www.google.com/calendar/feeds/"
   }
