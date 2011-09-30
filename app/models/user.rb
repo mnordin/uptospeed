a8db2013e810d9ee75a49f40e65660076cda4672
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 11:42:56 +0200

    Removed connections to the authentications model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
-  has_many :authentications, :dependent => :destroy
   has_many :events
 
   validates :email, :presence => true, :uniqueness => true
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.first_name = auth["user_info"]["first_name"]
-      user.first_name = auth["user_info"]["last_name"]
+      user.first_name = auth["user_info"]["name"]
       user.email = auth["user_info"]["email"]
-      user.authentications.find_or_create_by_provider_and_uid(auth["provider"], auth["uid"])
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
     end
   end
 
