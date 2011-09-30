1d94e7666b3776791b5cd4759187deb6b898e7d4
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 11:42:56 +0200

    Removed connections to the authentications model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ UpToSpeed::Application.routes.draw do
   match "/auth/:provider/callback" => "authentications#create"
   match "/auth/failure" => "authentications#failure", :as => "auth_failure"
 
-  resources :authentications, :users, :events
+  resources :users, :events
 
   root :to => 'events#index'
 end
