e32f7c368c9636f4863efe107b1e9f2deb025c0d
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 15:10:04 +0200

    Debbuging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,23 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     Rails.logger.info("********#{auth}")
-    @@auth = auth
-    session[:auth] = auth
     #user = User.find_by_provider_and_uid(params[:provider], auth["uid"]) || User.create_with_omniauth(auth)
     #session[:user_id] = user.id
-    #store_google_credentials(auth)
+    @@auth = auth
+    store_google_credentials(auth)
     redirect_to root_url, :notice => "Signed in!"
   end
 
   # Copy from create action
   def failure
-    Rails.logger.info("******** @@auth: #{@@auth}")
-    Rails.logger.info("********session[:auth] #{session[:auth]}")
-    
-    user = User.find_by_provider_and_uid("google", @@auth["uid"]) || User.create_with_omniauth(@@auth)
-    session[:user_id] = user.id
-    store_google_credentials(@@auth)
-    redirect_to root_url, :notice => "Signed in from #failure!"
+    #if get_google_credential.present?
+      Rails.logger.info("******** @@auth: #{@@auth}")
+      Rails.logger.info("********get_google_credential #{get_google_credential}")
+      store
+      user = User.find_by_provider_and_uid("google", @@auth["uid"]) || User.create_with_omniauth(@@auth)
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Signed in from #failure!"
+    #end
   end
 
   def destroy
@@ -33,5 +33,9 @@ class SessionsController < ApplicationController
   private
   def store_google_credentials(auth)
     session[:credentials] = auth["credentials"]
+  end
+
+  def get_google_credential
+    session[:credentials]
   end
 end
