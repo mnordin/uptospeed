38f4e2eb8b414aa7aa371d1396dde97f4fa44c6f
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 11:16:13 +0200

    Oauth key was wrong, adding :domain also

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.full_host = "http://uptospeed.se"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google, "newsdesk.se", "g_ysByjed_wnqipVx2bk8Quf", :scope => "https://www.google.com/calendar/feeds/" 
+  provider :google, "uptospeed.se", "g_ysByjed_wnqipVx2bk8Quf", :scope => "https://www.google.com/calendar/feeds/", :domain => "newsdesk.se"
 end
