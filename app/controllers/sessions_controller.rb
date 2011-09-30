8147f499641eb3b1b95d26020c975fa4e8e4b288
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 15:15:01 +0200

    Possible fix for error with validation from openid with ssl and heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,23 @@
 class SessionsController < ApplicationController
 
-  @@auth = {}
-
   def create
     auth = request.env["omniauth.auth"]
     Rails.logger.info("********#{auth}")
-    #user = User.find_by_provider_and_uid(params[:provider], auth["uid"]) || User.create_with_omniauth(auth)
-    #session[:user_id] = user.id
-    @@auth = auth
+    user = User.find_by_email(auth["user_info"]["email"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
     store_google_credentials(auth)
     redirect_to root_url, :notice => "Signed in!"
   end
 
-  # Copy from create action
   def failure
-    #if get_google_credential.present?
-      Rails.logger.info("******** @@auth: #{@@auth}")
-      Rails.logger.info("********get_google_credential #{get_google_credential}")
-      store
-      user = User.find_by_provider_and_uid("google", @@auth["uid"]) || User.create_with_omniauth(@@auth)
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Signed in from #failure!"
-    #end
+    if get_google_credentials.present?
+      redirect_to root_url
+    end
   end
 
   def destroy
     session[:user_id] = nil
-    session[:access_token] = nil
+    session[:credentials] = nil
     redirect_to root_path, :notice => "Logged out"
   end
 
@@ -35,7 +26,7 @@ class SessionsController < ApplicationController
     session[:credentials] = auth["credentials"]
   end
 
-  def get_google_credential
+  def get_google_credentials
     session[:credentials]
   end
 end
