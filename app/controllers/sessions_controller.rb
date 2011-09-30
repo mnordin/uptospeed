90dab428a432522673040f69f6836a6a03f988bc
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 14:34:35 +0200

    Google callback always seems to fail. set cookies before redirecting to failure action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,13 @@ class SessionsController < ApplicationController
     Rails.logger.info("********#{auth}")
     user = User.find_by_provider_and_uid(params[:provider], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    store_google_access_token(auth)
-    redirect_to root_url, :notice => "Signed in!"
+    store_google_credentials(auth)
+    #redirect_to root_url, :notice => "Signed in!"
   end
 
   # Copy from create action
   def failure
-    auth = request.env["omniauth.auth"]
-    Rails.logger.info("********#{auth}")
-    user = User.find_by_provider_and_uid("google", auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    store_google_credentials(auth)
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to root_url, :notice => "Signed in from #failure!"
   end
 
   def destroy
