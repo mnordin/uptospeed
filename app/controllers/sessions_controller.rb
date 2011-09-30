11f114478fdeed94f8989a363ba4e7021c6516d5
Markus Nordin <markus.nordin@gmail.com>
2011-09-30 11:54:48 +0200

    Added actions and routes to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    store_google_access_token(auth)
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:access_token] = nil
+    redirect_to root_path, :notice => "Logged out"
+  end
+
+  private
+  def store_google_access_token(auth)
+    session[:access_token] = auth["extra"]["access_token"].token
+  end
 end
