57237b041f114541a7c2665fa37eecd02e636290
Markus Nordin <markus.nordin@gmail.com>
2011-10-02 18:16:15 +0200

    Adding general and contacts scopes to try a different auth token

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.full_host = "http://uptospeed.se"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google, "uptospeed.se", "g_ysByjed_wnqipVx2bk8Quf", :scope => "https://www.google.com/calendar/feeds/", :domain => "newsdesk.se", :identifier => "https://www.google.com/accounts/o8/id", :callback_path => "/auth/google/callback"
-  provider :google_apps, nil, :scope => "https://www.google.com/calendar/feeds/", :domain => "newsdesk.se", :identifier => "https://www.google.com/accounts/o8/id", :callback_path => "/auth/google_apps/callback"
+  provider :google_apps, nil, :scope => "http://www.google.com/calendar/feeds/, http://www.google.com/base/feeds/, http://www.google.com/m8/feeds", :domain => "newsdesk.se", :identifier => "https://www.google.com/accounts/o8/id", :callback_path => "/auth/google_apps/callback"
 end
