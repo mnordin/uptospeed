f9c7318edf0b3f686c7aecc939b9c14c3b8f9093
Markus Nordin <markus.nordin@gmail.com>
2011-10-03 10:45:21 +0200

    Added getAccessToken which posts the auth token to google and hopefully responds with an access token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(auth["user_info"]["email"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     store_google_credentials(auth)
+    getAccessToken(auth["credentials"]["token"])
     redirect_to root_url, :notice => "Signed in!"
   end
 
@@ -22,8 +23,16 @@ class SessionsController < ApplicationController
   end
 
   private
+  def getAccessToken(oath_token)
+    sess = Patron::Session.new
+    sess.timeout = 10
+    response = sess.post("https://www.google.com/accounts/OAuthGetAccessToken", {:oauth_consumer_key => "g_ysByjed_wnqipVx2bk8Quf", :oauth_token => oath_token, :oauth_verifier => session["credentials"]["oauth_verifier"], :oauth_signature_method => "HMAC-SHA1", :oauth_signature => "g_ysByjed_wnqipVx2bk8Quf", :oauth_timestamp => Time.now.to_i, :oauth_nonce => Digest::MD5.hexdigest(rand.to_s)})
+    Rails.logger.info("********#{response}")
+  end
+
   def store_google_credentials(auth)
     session[:credentials] = auth["credentials"]
+    session[:credentials][:oauth_verifier] = params[:oauth_verifier]
   end
 
   def get_google_credentials
