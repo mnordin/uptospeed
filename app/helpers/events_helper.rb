1365be009b42a841d70c5e9ba82143ce4d06fb18
Markus Nordin <markus.nordin@gmail.com>
2011-10-04 23:17:57 +0200

    Recurring events are now handles as single events and displayed

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,17 @@
 module EventsHelper
+
+  def next_link
+    start_at = params[:start_at].present? ? params[:start_at] : Time.now.beginning_of_week
+    end_at = params[:end_at].present? ? params[:end_at] : Time.now.end_of_week
+    
+    return {:start_at => start_at + 1.week, :end_at => end_at + 1.week}
+  end
+
+  def prev_link
+    start_at = params[:start_at].present? ? params[:start_at] : Time.now.beginning_of_week
+    end_at = params[:end_at].present? ? params[:end_at] : Time.now.end_of_week
+    
+    return {:start_at => start_at - 1.week, :end_at => end_at - 1.week}
+  end
+
 end
