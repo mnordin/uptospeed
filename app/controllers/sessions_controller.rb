0876c564f667f8050c7d7fde638f5d858694a810
Markus Nordin <markus.nordin@gmail.com>
2011-10-06 00:09:22 +0200

    Removed old google credentials from session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,30 +5,14 @@ class SessionsController < ApplicationController
     Rails.logger.info("********#{auth}")
     user = User.find_by_email(auth["user_info"]["email"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    store_google_credentials(auth)
     redirect_to root_url, :notice => "Signed in!"
   end
 
   def failure
-    if get_google_credentials.present?
-      redirect_to root_url
-    end
   end
 
   def destroy
     session[:user_id] = nil
-    session[:credentials] = nil
     redirect_to root_path, :notice => "Logged out"
-  end
-
-  private
-
-  def store_google_credentials(auth)
-    session[:credentials] = auth["credentials"]
-    session[:credentials][:oauth_verifier] = params[:oauth_verifier]
-  end
-
-  def get_google_credentials
-    session[:credentials]
   end
 end
