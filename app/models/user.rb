5cae927ad21ccc1819ff845b47c8eae5351eb4df
Markus Nordin <markus.nordin@gmail.com>
2011-10-06 00:08:49 +0200

    Since changing back to unscoped auth, user model can save first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ActiveRecord::Base
 
   validates :email, :presence => true, :uniqueness => true
 
+  def name
+    "#{first_name} #{last_name}"
+  end
+
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.name = auth["user_info"]["name"]
+      user.first_name = auth["user_info"]["first_name"]
+      user.last_name = auth["user_info"]["last_name"]
       user.email = auth["user_info"]["email"]
       user.provider = auth["provider"]
       user.uid = auth["uid"]
