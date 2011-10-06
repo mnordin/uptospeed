46f8ede670c35d2916b12b3362939ae898cb3085
Markus Nordin <markus.nordin@gmail.com>
2011-10-06 14:27:02 +0200

    Forces users to log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # sessionscontroller uses create action to set user_id
+  before_filter :require_authed_user, :except => :create
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      require_authed_user
+    end
+  end
+
+  protected
+  def require_authed_user
+    if session[:user_id].blank?
+      redirect_to "/auth/google_apps"
+    end
   end
 end
