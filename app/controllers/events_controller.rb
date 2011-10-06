f3cc4a78cded1c8a33005812c9669812ade2c814
Markus Nordin <markus.nordin@gmail.com>
2011-10-06 14:27:02 +0200

    Forces users to log in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-  #before_filter :require_authed_user, :only => [:index]
 
   def index
     start_at = params[:start_at].present? ? params[:start_at] : Time.now.beginning_of_week
