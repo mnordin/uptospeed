5458244e86190be44fc6d616e7eb9467ad3e32ba
Markus Nordin <markus.nordin@gmail.com>
2011-10-06 14:01:31 +0200

    Attend or unattend on google now uses delayed_job

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,7 +17,7 @@ class Event < ActiveRecord::Base
       "ctz"                         => "Europe/Stockholm",
       "singleevents"                => true
     })
-    calendar_id = Event.fetch_up_to_speed_calendar(service).id
+    calendar_id = Event.fetch_calendar(service).id
     events.select!{|e| e.calendar_id == CGI.escape("newsdesk.se_tr45ab3op4eh54497ko4073118@group.calendar.google.com")}.sort!{|a,b| a.start_time <=> b.start_time}
     events.each do |event|
       event.attendees.delete_if{|a| a[:name] =~ /Up to Speed Stockholm/}
@@ -30,11 +30,28 @@ class Event < ActiveRecord::Base
     return service
   end
 
-  def self.fetch_up_to_speed_calendar(service = nil)
+  def self.fetch_calendar(service = nil)
     unless service.present?
       service = auth_google_service
     end
     service.calendars.select{|cal| cal.title =~ /Up to Speed Stockholm/}.first
     # or id "newsdesk.se_tr45ab3op4eh54497ko4073118@group.calendar.google.com"
+  end
+
+
+  def self.attend_on_google(google_id, user)
+    service = Event.auth_google_service
+    google_event = GCal4Ruby::Event.find(service, :id => google_id)
+    google_event.attendees << {:email => user.email, :name => user.name, :role => "attendee", :status => "accepted"}
+    google_event.calendar = Event.fetch_calendar(service)
+    google_event.save
+  end
+
+  def self.unattend_on_google(google_id, user)
+    service = Event.auth_google_service
+    google_event = GCal4Ruby::Event.find(service, :id => google_id)
+    google_event.attendees.delete_if {|u| u[:email] == user.email}
+    google_event.calendar = Event.fetch_calendar(service)
+    google_event.save
   end
 end
