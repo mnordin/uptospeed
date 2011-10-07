c4e9e257ad930b313470dc14f972e2ad6a32b4f6
Markus Nordin <markus.nordin@gmail.com>
2011-10-07 09:45:00 +0200

    Added a helper for getting the url friendly monday date

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,6 +12,10 @@ module EventsHelper
     return {:start_at => start_at.strftime("%Y-%m-%d")}
   end
 
+  def monday_param
+    params[:start_at].present? ? params[:start_at] : Time.now.beginning_of_week.strftime("%Y-%m-%d")
+  end
+
 
   # Weekdays helpers
   def which_weekday?(event, day)
