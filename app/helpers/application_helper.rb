85e5e649c74403f6689e3216ffead23f4679c284
Markus Nordin <markus.nordin@gmail.com>
2011-10-07 17:52:46 +0200

    Main navigation highlights the current tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     User.find(session[:user_id]) if session[:user_id]
   end
 
+  def highlight_current_tab(controller_name)
+    if controller_name == params[:controller]
+      return "ui-btn-active"
+    else
+      ""
+    end
+  end
+
 end
