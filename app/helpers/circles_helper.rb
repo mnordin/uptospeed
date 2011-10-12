dbf99b244938acd8eeb2e04091445417d73cd2a2
Markus Nordin <markus.nordin@gmail.com>
2011-10-12 21:34:20 +0200

    Added user_score helper

--- app/helpers/circles_helper.rb
+++ app/helpers/circles_helper.rb
@@ -1,2 +1,10 @@
 module CirclesHelper
+
+  def user_score(circle_memberships, user)
+    if circle_memberships.select{|cm| cm.user == user}.first.share_score?
+      user.total_score
+    else
+      ""
+    end
+  end
 end
