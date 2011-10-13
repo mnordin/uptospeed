0666de9450d0882bb9988bd7684fd77f7ac3fd36
Markus Nordin <markus.nordin@gmail.com>
2011-10-13 16:04:09 +0200

    Added methods for figuring out the state of the membership status

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -5,4 +5,16 @@ class Circle < ActiveRecord::Base
   accepts_nested_attributes_for :circle_memberships, :reject_if => lambda { |cm| cm[:user_id].blank? }, :allow_destroy => true
 
   validates :title, :presence => true
+
+  def accepted?(user)
+    circle_memberships.select{|cm| cm.user == user }.first.accepted? == true
+  end
+
+  def accepted_users
+    users.select{|u| u.has_accepted_membership?(self) }
+  end
+
+  def pending_users
+    users.select{|u| u.hasnt_accepted_membership?(self) }
+  end
 end
