a362811aefc20fb64bafd3c11a82f8d73b88f624
Markus Nordin <markus.nordin@gmail.com>
2011-10-14 11:29:58 +0200

    Removed unused helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -20,21 +20,4 @@ module EventsHelper
     (Time.parse(param).end_of_day + wday.days) < Time.now
   end
 
-  # Weekdays helpers
-  def which_weekday?(event, day)
-    weekdays = {
-      1 => "monday",
-      2 => "tuesday",
-      3 => "wednesday",
-      4 => "thursday",
-      5 => "friday"
-    }
-    event_day = weekdays[event.start_time.wday]
-    if event_day == day
-      return true
-    else
-      return false
-    end
-  end
-
 end
