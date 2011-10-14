5ee9240a8e7b818d28bfc1a39548b0a460417205
Markus Nordin <markus.nordin@gmail.com>
2011-10-14 13:06:47 +0200

    Upgraded jquery mobile to bleeding edge 1.0 rc2pre

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,6 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require "jquery-1.6.4"
+//= require "jquery.mobile-1.0rc2pre"
 //= require_tree .
