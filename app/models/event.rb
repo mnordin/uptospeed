d7b59e8ff0d6f3c1f23059e4758c0271f6bf0d57
Markus Nordin <markus.nordin@gmail.com>
2011-10-14 11:30:17 +0200

    Refactored event list to user group_by date

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,6 +12,10 @@ class Event < ActiveRecord::Base
     start_time < Time.now
   end
 
+  def group_by_date
+    start_time.to_date
+  end
+
   def self.exists_before?(time)
     Event.order(:start_time).limit(1).first.start_time < Time.parse(time).beginning_of_week
   end
