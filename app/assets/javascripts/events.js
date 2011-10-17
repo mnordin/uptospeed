912879a95afc8c3e6cf0bf83d3a243aa365325c2
Markus Nordin <markus.nordin@gmail.com>
2011-10-17 22:57:07 +0200

    Implemented bug fixes for google map

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,26 +1,15 @@
-// $("#events-show").live("pagecreate", function(){
-//     var map_element = $("div[data-role='map']", this);
-//     var lat = $(map_element).data("lat");
-//     var lng = $(map_element).data("lng");
-//     init_map(lat, lng, map_element);
-// });
-
-function init_map(lat, lng, element){
-    var latlng = new google.maps.LatLng(lat, lng);
-    var myOptions = {
-        zoom: 15,
-        center: latlng,
-        disableDefaultUI: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(element[0], myOptions);
-    resize_map(map, latlng);
-    var marker = new google.maps.Marker({
-        position: latlng, 
-        map: map
+var latlng;
+$("#events-map").live("pagecreate", function() {
+    var lat = $("#map").data("lat");
+    var lng = $("#map").data("lng");
+    latlng = new google.maps.LatLng(lat, lng);
+    $("#map").gmap({
+        "zoom": 15,
+        "center": latlng
     });
-};
-
-function resize_map(map, latlng){
-    google.maps.event.trigger(map, 'resize');
-}
+});
+$("#events-map").live("pageshow", function() {
+    $("#map").gmap('refresh');
+    $("#map").gmap('addMarker', { 'position': latlng } );
+    $("#map").gmap({"center": latlng});
+});
