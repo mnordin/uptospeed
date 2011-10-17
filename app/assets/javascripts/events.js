a5ea11c61c2d07b124edebda4072817828ba648e
Markus Nordin <markus.nordin@gmail.com>
2011-10-17 13:45:06 +0200

    Implementing google map (still resize bug)

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,13 +1,28 @@
-function init_map(lat, lng) {
+$("#events-show").live("pageinit", function(){
+    var map = $("div[data-role='map']", this);
+    var lat = $(map).data("lat");
+    var lng = $(map).data("lng");
+    init_map(lat, lng, map);
+});
+
+function init_map(lat, lng, element){
     var latlng = new google.maps.LatLng(lat, lng);
     var myOptions = {
-        zoom: 16,
+        zoom: 15,
         center: latlng,
+        disableDefaultUI: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    var map = new google.maps.Map(element[0], myOptions);
+    resize_map(map, latlng);
     var marker = new google.maps.Marker({
         position: latlng, 
         map: map
     });
 };
+
+function resize_map(map, latlng){
+    map.setCenter(latlng);
+    map.setZoom(map.getZoom());
+    google.maps.event.trigger(map, 'resize');
+}
