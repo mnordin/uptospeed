001736dda40bab0f3326534bee45e81fef9d9b60
Markus Nordin <markus.nordin@gmail.com>
2011-10-18 16:22:16 +0200

    Removed current_user from helper and defined controller action as helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def current_user
-    User.find(session[:user_id]) if session[:user_id]
-  end
-
   def highlight_current_tab(controller_name)
     if controller_name == params[:controller]
       return "ui-btn-active"
