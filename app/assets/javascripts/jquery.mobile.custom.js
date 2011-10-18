f723857e80c30f60dbd223adc47646120d1c44f8
Markus Nordin <markus.nordin@gmail.com>
2011-10-18 11:11:52 +0200

    Added a custom settings file for jquery mobile

---
+++ app/assets/javascripts/jquery.mobile.custom.js
@@ -0,0 +1,6 @@
+$(document).bind("mobileinit", function(){
+    $.mobile.touchOverflowEnabled = true;
+    $.mobile.loadingMessage = "laddar";
+    $.mobile.pageLoadErrorMessage = "Det gick inte att ladda sidan";
+  
+});
