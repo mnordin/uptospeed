e910d4a18c391b13ce0b0d28c72720d91836cd86
Markus Nordin <markus.nordin@gmail.com>
2011-10-19 23:51:48 +0200

    Removed console logs from circles javascript

--- app/assets/javascripts/circles.js
+++ app/assets/javascripts/circles.js
@@ -1,8 +1,5 @@
 $("#circles-edit").live("pageshow", function(){
-    console.log("baaa");
     $("input[type='checkbox'][checked='checked']").each(function(){
-        console.log($(this));
-        console.log("hej");
         $(this).checkboxradio('disable');
     });
 });
