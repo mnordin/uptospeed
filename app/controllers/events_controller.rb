f72d860c9c732fb4e66dce8dee7a831adb38637d
Markus Nordin <markus.nordin@gmail.com>
2011-10-19 11:09:13 +0200

    Events are now listed depending on today instead of the week
    5 days ahead, 5 days back on each index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,8 @@
 class EventsController < ApplicationController
 
   def index
-    future_weeks = params[:future_weeks].try(:to_i) || 1
-    past_weeks = params[:past_weeks].try(:to_i) || 1
-    @events_grouped = Event.where("start_time > ? and end_time < ?", (Time.now - past_weeks.week).beginning_of_day, (Time.now + future_weeks.week).end_of_day).order("start_time DESC").group_by(&:group_by_date)
+    now = params[:now].present? ? Time.parse(params[:now]) : Time.now
+    @events_grouped = Event.where("start_time > ? and end_time < ?", (now - 5.days).beginning_of_day, (now + 5.days).end_of_day).order("start_time DESC").group_by(&:group_by_date)
   end
 
   def show
