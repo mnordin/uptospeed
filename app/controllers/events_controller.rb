68762df2316e23250cd8ca668e69b73364b081ca
Markus Nordin <markus.nordin@gmail.com>
2011-10-20 12:05:37 +0200

    Event list are now based on weeks again. Weekends should list the following week also. Not 100% foolproof just yet though

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,24 @@ class EventsController < ApplicationController
 
   def index
     now = params[:now].present? ? Time.parse(params[:now]) : Time.now
-    @events_grouped = Event.where("start_time > ? and end_time < ?", (now - 5.days).beginning_of_day, (now + 5.days).end_of_day).order("start_time ASC").group_by(&:group_by_date)
-    unless @events_grouped.has_key? Time.now.to_date
-      today = {Time.now.to_date => []}
-      @events_grouped.merge!(today)
-      new_hash = {}
-      @events_grouped.keys.sort.map{|date| new_hash[date] = @events_grouped[date]}
-      @events_grouped = new_hash
+    if weekend?(now)
+      start_date = now.strftime("%A").downcase == "saturday" ? now : now - 1.days
+      start_date = start_date.beginning_of_day
+      end_date = now.end_of_week
+    else
+      start_date = now.beginning_of_week
+      end_date = now.end_of_week
+    end
+    @events_grouped = Event.where("start_time > ? and end_time < ?", start_date, end_date).order("start_time ASC").group_by(&:group_by_date)
+    if !@events_grouped.has_key?(Time.now.to_date)
+      date_range = (@events_grouped.keys.first..@events_grouped.keys.last)
+      if date_range.cover?(Time.now.to_date)
+        today = {Time.now.to_date => []}
+        @events_grouped.merge!(today)
+        new_hash = {}
+        @events_grouped.keys.sort.map{|date| new_hash[date] = @events_grouped[date]}
+        @events_grouped = new_hash
+      end
     end
   end
 
@@ -26,5 +37,10 @@ class EventsController < ApplicationController
     else
       redirect_to params[:url], notice: 'Nagot gick fel, forsok igen'
     end
+  end
+
+  private
+  def weekend?(date)
+    date.wday == 6 or date.wday == 0
   end
 end
