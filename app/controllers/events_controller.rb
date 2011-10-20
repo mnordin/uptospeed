dc34106175efc31c7cd1aef264d397054ca8aaf9
Markus Nordin <markus.nordin@gmail.com>
2011-10-20 10:26:55 +0200

    Changed events index to inject todays date if it isn't included in the hash of events for index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,14 @@ class EventsController < ApplicationController
 
   def index
     now = params[:now].present? ? Time.parse(params[:now]) : Time.now
-    @events_grouped = Event.where("start_time > ? and end_time < ?", (now - 5.days).beginning_of_day, (now + 5.days).end_of_day).order("start_time DESC").group_by(&:group_by_date)
+    @events_grouped = Event.where("start_time > ? and end_time < ?", (now - 5.days).beginning_of_day, (now + 5.days).end_of_day).order("start_time ASC").group_by(&:group_by_date)
+    unless @events_grouped.has_key? Time.now.to_date
+      today = {Time.now.to_date => []}
+      @events_grouped.merge!(today)
+      new_hash = {}
+      @events_grouped.keys.sort.map{|date| new_hash[date] = @events_grouped[date]}
+      @events_grouped = new_hash
+    end
   end
 
   def show
