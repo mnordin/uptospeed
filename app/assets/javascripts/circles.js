6a5f58007c4075eca3e4157be814974bf3757f00
Markus Nordin <markus.nordin@gmail.com>
2011-10-25 15:09:09 +0200

    Changed js for new form, jqm needs disabled attribute but the form rejects the value. remove the disabled attribute after jqm initialization

--- app/assets/javascripts/circles.js
+++ app/assets/javascripts/circles.js
@@ -1,5 +1,15 @@
-$("#circles-edit, #circles-new").live("pageshow", function(){
+// Inviting new members should disable the current members and/or already invited
+$("#circles-edit").live("pageshow", function(){
     $("input[type='checkbox'][checked='checked']").each(function(){
         $(this).checkboxradio('disable');
+    });
+});
+
+// Creating a new circle should hide self to keep from creating a circle
+// where a user can choose to be in it or not
+$("#circles-new").live("pageshow", function(){
+    $("input[type='checkbox'][checked='checked']").each(function(){
+        $(this).checkboxradio('disable');
+        $(this).removeAttr("disabled");
     });
 });
