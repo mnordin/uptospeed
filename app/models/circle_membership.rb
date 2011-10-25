83211be66975f806d05bf1fbf009cb5f6178c935
Markus Nordin <markus.nordin@gmail.com>
2011-10-25 15:08:06 +0200

    If there are no more accepted memberships, the circle is destroyed

--- app/models/circle_membership.rb
+++ app/models/circle_membership.rb
@@ -2,4 +2,12 @@ class CircleMembership < ActiveRecord::Base
   belongs_to :user
   belongs_to :circle
   belongs_to :color
+
+  after_destroy :destroy_circle_if_last_membership
+
+  private
+  def destroy_circle_if_last_membership
+    circle.destroy if circle.accepted_users.count == 0
+  end
+
 end
