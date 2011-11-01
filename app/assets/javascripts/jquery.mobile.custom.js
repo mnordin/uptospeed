ba6c7f38463b1d8a0ae46e0dfaf7b29dcf0ff07e
Markus Nordin <markus.nordin@gmail.com>
2011-11-01 21:54:32 +0100

    Enabled 3D hardware enabled transitions (buggy support)

--- app/assets/javascripts/jquery.mobile.custom.js
+++ app/assets/javascripts/jquery.mobile.custom.js
@@ -1,4 +1,5 @@
 $(document).bind("mobileinit", function(){
     $.mobile.loadingMessage = "laddar";
     $.mobile.pageLoadErrorMessage = "Det gick inte att ladda sidan";
+    $.mobile.touchOverflowEnabled = true;
 });
