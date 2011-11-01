69049625b01ed61d28a4a255be251abea47eaf24
Markus Nordin <markus.nordin@gmail.com>
2011-11-01 23:55:22 +0100

    Hacky solution to always set static title

--- app/assets/javascripts/jquery.mobile.custom.js
+++ app/assets/javascripts/jquery.mobile.custom.js
@@ -2,4 +2,7 @@ $(document).bind("mobileinit", function(){
     $.mobile.loadingMessage = "laddar";
     $.mobile.pageLoadErrorMessage = "Det gick inte att ladda sidan";
     $.mobile.touchOverflowEnabled = true;
+
+    // hacky solution to always set the title to Up to Speed
+    $('div[data-role="page"]').live("pageshow",function(){document.title = "Up to Speed";});
 });
