20dd2005f09aca1dc5fb71d2f3c15b08cf6a81bf
Markus Nordin <markusnordin@Martins-mbp.local>
2011-11-02 17:21:14 +0100

    Removed migrations not needed (discovered during a fresh install)

--- db/migrate/20111005132208_remove_user_id_field_from_events.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFieldFromEvents < ActiveRecord::Migration
-  def up
-    remove_column :events, :user_id
-  end
-
-  def down
-    add_column :events, :user_id, :integer
-  end
-end
--- db/migrate/20111013122402_rename_pending_to_accepted_for_circle_memberships.rb
+++
@@ -1,6 +0,0 @@
-class RenamePendingToAcceptedForCircleMemberships < ActiveRecord::Migration
-  def change
-    rename_column :circle_memberships, :pending, :accepted
-    change_column_default :circle_memberships, :accepted, false
-  end
-end
