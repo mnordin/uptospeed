d0c0d8e92e8e80f263bcf1029e7bc8b34f48e7c5
Markus Nordin <markusnordin@Martins-mbp.local>
2011-11-02 17:21:14 +0100

    Removed migrations not needed (discovered during a fresh install)

--- db/migrate/20111013121507_set_default_value_on_pending_circle_membership.rb
+++
@@ -1,9 +0,0 @@
-class SetDefaultValueOnPendingCircleMembership < ActiveRecord::Migration
-  def up
-    change_column_default(:circle_memberships, :pending, true)
-  end
-
-  def down
-    change_column_default(:circle_memberships, :pending, nil)
-  end
-end
