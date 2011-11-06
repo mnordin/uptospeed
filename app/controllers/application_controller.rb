940195acdd6284def8b360bc82af17f2df5ba1e8
Markus Nordin <markus.nordin@gmail.com>
2011-11-06 18:48:12 +0100

    Defined user as a variable to save database lookups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      User.find(session[:user_id])
+      @user ||= User.find(session[:user_id])
     else
       require_authed_user
     end
