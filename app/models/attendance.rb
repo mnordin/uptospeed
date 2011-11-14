91493ee6234e0960010444fa7a397565a60d89bc
Markus Nordin <markus.nordin@gmail.com>
2011-11-14 21:50:32 +0100

    Removed functionality handled by DCI roles

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,20 +2,9 @@ class Attendance < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
 
-  after_create :enqueue_attend_on_google
-  after_destroy :enqueue_unattend_on_google
-
   # A user can't attend the same event more than once
   validates :user_id, :presence => true, :uniqueness => { :scope => :event_id }
   validates :event_id, :presence => true
-
-  def enqueue_attend_on_google
-    Event.delay.attend_on_google(event.google_id, user)
-  end
-
-  def enqueue_unattend_on_google
-    Event.delay.unattend_on_google(event.google_id, user)
-  end
 
   def self.create_or_destroy_by_params(params)
     service = Event.auth_google_service
