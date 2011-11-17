da35a4782a8411579e47811150e8ed50d5e857c6
Markus Nordin <markus.nordin@gmail.com>
2011-11-17 20:22:56 +0100

    Added contexts and roles folders to config.autoload

--- app/contexts/user_attends_event_context.rb
+++ app/contexts/user_attends_event_context.rb
@@ -8,25 +8,21 @@ class UserAttendsEventContext
   end
 
   def attend_event
-    require File.dirname(__FILE__) + '/../roles/users/attending_role.rb'
-    user.extend AttendingRole
+    user.extend Users::AttendingRole
     user.attend(event)
 
     # if event.public?
-    require File.dirname(__FILE__) + '/../roles/events/public_event_role.rb'
-    event.extend PublicEventRole
+    event.extend Events::PublicEventRole
     event.attend_on_google(user)
     # end
   end
 
   def unattend_event
-    require File.dirname(__FILE__) + '/../roles/users/attending_role.rb'
-    user.extend AttendingRole
+    user.extend Users::AttendingRole
     user.unattend(event)
 
     # if event.public?
-    require File.dirname(__FILE__) + '/../roles/events/public_event_role.rb'
-    event.extend PublicEventRole
+    event.extend Events::PublicEventRole
     event.unattend_on_google(user)
     # end
   end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module UpToSpeed
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/app/contexts)
+    config.autoload_paths += %W(#{config.root}/app/roles)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
