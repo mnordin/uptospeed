a2c0316cb3b45aaf7cd5f77b55dbd64a4e16efcf
Markus Nordin <markus.nordin@gmail.com>
2011-11-18 16:23:42 +0100

    Removed attendance class methods

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,19 +6,6 @@ class Attendance < ActiveRecord::Base
   validates :user_id, :presence => true, :uniqueness => { :scope => :event_id }
   validates :event_id, :presence => true
 
-  def self.create_or_destroy_by_params(params)
-    if params[:attending] == "true"
-      unless Attendance.already_exists?(params)
-        Attendance.create(:user_id => params[:user_id], :event_id => params[:id])
-      end
-    else
-      if Attendance.already_exists?(params)
-        a = Attendance.find_by_user_id_and_event_id(params[:user_id], params[:id])
-        a.destroy
-      end
-    end
-  end
-
   def self.already_exists?(params)
     Attendance.find_by_user_id_and_event_id(params[:user_id], params[:id]).present?
   end
