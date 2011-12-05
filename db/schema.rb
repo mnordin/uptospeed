0866ad5115566e72861376e4d9f5efb875acbc22
Markus Nordin <markus.nordin@gmail.com>
2011-12-05 14:47:55 +0100

    Added locale string to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111205124840) do
+ActiveRecord::Schema.define(:version => 20111205133057) do
 
   create_table "attendances", :force => true do |t|
     t.integer  "user_id"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20111205124840) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "office_id"
+    t.string   "locale"
   end
 
 end
