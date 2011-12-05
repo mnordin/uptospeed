292e347773db17bde805acc0ef1b8a835962921b
Markus Nordin <markus.nordin@gmail.com>
2011-12-05 10:26:38 +0100

    dropped delayed_job table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111117192813) do
+ActiveRecord::Schema.define(:version => 20111205092451) do
 
   create_table "attendances", :force => true do |t|
     t.integer  "user_id"
@@ -50,19 +50,6 @@ ActiveRecord::Schema.define(:version => 20111117192813) do
   create_table "colors", :force => true do |t|
     t.string   "hex"
     t.string   "color"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.text     "locked_by"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
