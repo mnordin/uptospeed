477fce1115aec3650aa4cba4c59d2f4d414847f9
Markus Nordin <markus.nordin@gmail.com>
2011-12-06 15:34:11 +0100

    current_user variable is more verbose and less prone to be overridden later on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @user ||= User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     else
       require_authed_user
     end
