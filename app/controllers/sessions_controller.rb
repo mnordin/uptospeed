b9b25d92079e7d45d772e9c4e0d6bac692b672c2
Markus Nordin <markus.nordin@gmail.com>
2011-12-06 15:32:53 +0100

    If user registers for the first time, redirect to edit user form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
   # sign in using omniauth
   def sign_in
     auth = request.env["omniauth.auth"]
-    user = User.find_by_email(auth["user_info"]["email"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    redirect_to root_path, :notice => "Signed in!"
+    if user = User.find_by_email(auth["user_info"]["email"])
+      session[:user_id] = user.id
+      redirect_to root_path, :notice => "Signed in!"
+    else
+      user = User.create_with_omniauth(auth)
+      session[:user_id] = user.id
+      redirect_to edit_user_path(user), :notice => "Welcome! Please enter your information"
+    end
   end
 
   def failure
