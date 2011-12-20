12567b619a175dad7b00b40644d54e721301f69a
Markus Nordin <markus.nordin@gmail.com>
2011-12-20 20:17:43 +0100

    exclude asset path from fingerprinting digest

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module UpToSpeed
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Exclude specific assets from fingerprinting (use a path, glob, regex or proc)
+    config.assets.digest_exclusions << "assets/*"
   end
 end
