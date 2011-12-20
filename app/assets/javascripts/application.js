5550a3b24f1bc1564cc5d7bee725e35a556cf3c9
Markus Nordin <markus.nordin@gmail.com>
2011-12-20 21:13:15 +0100

    added a javascript listener for updated manifests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,11 @@
 //= require "jquery.mobile-1.0"
 //= require "jquery.ui.map.full.min"
 //= require_tree .
+
+if (window.applicationCache) {
+    applicationCache.addEventListener('updateready', function() {
+        if (confirm('An update is available. Reload now?')) {
+            window.location.reload();
+        }
+    });
+}
