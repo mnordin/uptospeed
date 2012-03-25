fbd16d4bc72cc9a2c8698a9d2e387667d3d19456
Markus Nordin <markus.nordin@gmail.com>
2012-03-25 22:00:48 +0200

    Set expiration date in config setting instead

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-UpToSpeed::Application.config.session_store :cookie_store, :key => '_up-to-speed_session'
-
+UpToSpeed::Application.config.session_store :cookie_store, :key => '_up-to-speed_session', :expire_after => 6.months
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
