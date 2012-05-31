58059b632677cd5b7d84f1818e1a6b7e98d0d931
Markus Nordin <markus.nordin@gmail.com>
2012-05-31 10:21:07 +0200

    Fix for creating Learning with wrong params

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -1,6 +1,6 @@
 class LearningsController < ApplicationController
   def create
-    if Learning.create(params[:read])
+    if Learning.create(params[:learning])
       flash = { :success => I18n.t(:'reads.success') }
     else
       flash = { :error => I18n.t(:'reads.error') }
