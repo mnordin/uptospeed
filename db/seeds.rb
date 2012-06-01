28559f0158b6c49cfb11b8736e4fb795bdb75697
Markus Nordin <markus.nordin@gmail.com>
2012-06-01 14:29:35 +0200

    Remove Colors from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
-Color.create(:hex => "transparent", :color => "none")
-Color.create(:hex => "#f52a2a", :color => "red")
-Color.create(:hex => "#ffb545", :color => "orange")
-Color.create(:hex => "#fce917", :color => "yellow")
-Color.create(:hex => "#91f016", :color => "lime")
-Color.create(:hex => "#04c71b", :color => "green")
-Color.create(:hex => "#42bdff", :color => "light_blue")
-Color.create(:hex => "#1842d9", :color => "blue")
-Color.create(:hex => "#e61ebe", :color => "purple")
-
 
 # ------------ OFFICES ------------
 
