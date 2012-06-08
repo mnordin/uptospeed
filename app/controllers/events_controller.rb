dc21e35fbb7b2d26f4a293643452dabdbb88f24a
Markus Nordin <markus.nordin@gmail.com>
2012-06-08 11:32:40 +0200

    Fixed bug where user may no belong to office

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,15 +10,17 @@ class EventsController < ApplicationController
       start_date = now.beginning_of_week
       end_date = now.end_of_week
     end
-    @events_grouped = current_user.office.events.where("start_time > ? and end_time < ?", start_date, end_date).order("start_time ASC").group_by(&:group_by_date)
-    if !@events_grouped.has_key?(Time.now.to_date) && @events_grouped.present?
-      date_range = (start_date.to_date..@events_grouped.keys.last)
-      if date_range.cover?(Time.now.to_date)
-        today = {Time.now.to_date => []}
-        @events_grouped.merge!(today)
-        new_hash = {}
-        @events_grouped.keys.sort.map{|date| new_hash[date] = @events_grouped[date]}
-        @events_grouped = new_hash
+    if current_user.office.present?
+      @events_grouped = current_user.office.events.where("start_time > ? and end_time < ?", start_date, end_date).order("start_time ASC").group_by(&:group_by_date)
+      if !@events_grouped.has_key?(Time.now.to_date) && @events_grouped.present?
+        date_range = (start_date.to_date..@events_grouped.keys.last)
+        if date_range.cover?(Time.now.to_date)
+          today = {Time.now.to_date => []}
+          @events_grouped.merge!(today)
+          new_hash = {}
+          @events_grouped.keys.sort.map{|date| new_hash[date] = @events_grouped[date]}
+          @events_grouped = new_hash
+        end
       end
     end
     @learning = Learning.new(:user => current_user)
