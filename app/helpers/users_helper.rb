a5ea6768c3451c3c3c00d5b759573f6c49ffef27
Markus Nordin <markus.nordin@gmail.com>
2012-06-08 10:33:13 +0200

    Made level point threshold exponential
    Max level is 10, first cash out is at 7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   def level_theme(level)
-    if level.level < 6
+    if level.level < 7
       return "ui-body-f"
-    elsif level.level < 9
+    elsif level.level < 10
       return "ui-body-e"
     else
       return "ui-body-g"
--- lib/uptospeed/level.rb
+++ lib/uptospeed/level.rb
@@ -10,15 +10,16 @@ class Level
 
   def to_level
     case @points
-      when 0..39 then return 1, 0, 40
-      when 40..89 then return 2, 40, 90
-      when 90..159 then return 3, 90, 160
-      when 160..249 then return 4, 160, 250
-      when 250..374 then return 5, 250, 375
-      when 375..499 then return 6, 375, 500 # first cashout
-      when 500..624 then return 7, 500, 625
-      when 625..750 then return 8, 625, 750
-      when 750..9999 then return 9, 750, 0 # second cashout
+      when 0..8 then return 1, 0, 9
+      when 9..37 then return 2, 9, 38
+      when 38..84 then return 3, 38, 85
+      when 85..149 then return 4, 85, 150
+      when 150..234 then return 5, 150, 235
+      when 235..374 then return 6, 235, 375
+      when 375..461 then return 7, 375, 462 # first cashout
+      when 462..619 then return 8, 462, 620
+      when 620..749 then return 9, 620, 750
+      when 750..9999 then return 10, 749, 0 # second cashout
     end
   end
 end
