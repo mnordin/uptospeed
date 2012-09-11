8cce0be561b325ed976a416cadf23bf7f1b37b14
Markus Nordin <markus.nordin@gmail.com>
2012-09-11 13:09:53 +0200

    Scopes for this month

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,7 @@ class User < ActiveRecord::Base
   end
 
   def events_learnings_and_workouts_this_month
-    ((workouts.past_this_month) +
-    learnings.where({:created_at => Time.now.beginning_of_month..Time.now.end_of_month}) +
-    events.where({:start_time => Time.now.beginning_of_month..Time.now.end_of_month})).sort { |o, p| o.created_at <=> p.created_at }.reverse
+    (workouts.this_month + learnings.this_month + events.past_this_month).reverse
   end
 
   def all_events_learnings_and_workouts
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,8 @@ class Workout < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :presence => true
 
+  scope :this_month, lambda { where("created_at > ?", Time.now.beginning_of_month) }
+
   def title
     self.class.to_s
   end
