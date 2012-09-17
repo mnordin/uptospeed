ef2f41234647a1505eafb39e2e212f63278600de
Markus Nordin <markus.nordin@gmail.com>
2012-09-17 22:25:54 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 UpToSpeed::Application.routes.draw do
 
-  match "/auth/:provider/callback" => "sessions#sign_in"
-  match "/auth/failure" => "sessions#failure"
-  match "/signout" => "sessions#sign_out", :as => :signout
+  match '/auth/:provider/callback' => 'sessions#sign_in'
+  match '/auth/failure' => 'sessions#failure'
+  match '/signout' => 'sessions#sign_out', :as => :signout
 
-  match "/users/:id/levels_info" => "users#levels_info", :as => :levels_info
+  resources :users do
+    get 'levels_info', :on => :member
+  end
 
-  match "/events/:id/map" => "events#map", :only => "get", :as => :event_map
-  match "/events/:id/attend" => "events#attend", :only => "post", :as => :event_attend
+  resources :events do
+    member do
+      get 'map'
+      post 'register_attendees'
+      post 'attend'
+    end
+  end
 
-  resources :users, :events, :learnings, :workouts
+  resources :learnings, :workouts
 
   root :to => 'events#index'
 end
