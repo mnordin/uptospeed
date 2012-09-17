ff134b36ccd862105316fe26091b00241f353fd3
Markus Nordin <markus.nordin@gmail.com>
2012-09-17 23:39:53 +0200

    New attendees view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ UpToSpeed::Application.routes.draw do
   resources :events do
     member do
       get 'map'
+      get 'new_attendees'
       post 'register_attendees'
       post 'attend'
     end
