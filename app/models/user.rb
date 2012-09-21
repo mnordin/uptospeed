35abdaa1aa852f0e942b120ed673254b4ec0eb42
Markus Nordin <markus.nordin@gmail.com>
2012-09-21 13:27:48 +0200

    Fix for new omniauth hash when creating new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,9 @@ class User < ActiveRecord::Base
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.first_name = auth["user_info"]["first_name"]
-      user.last_name = auth["user_info"]["last_name"]
-      user.email = auth["user_info"]["email"]
+      user.first_name = auth["info"]["first_name"]
+      user.last_name = auth["info"]["last_name"]
+      user.email = auth["info"]["email"]
       user.provider = auth["provider"]
       user.uid = auth["uid"]
     end
